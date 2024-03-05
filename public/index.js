3d001456e537ddf48c0e3516eff752b4848d0a46
William Gregorio <gregorio@Williams-MacBook-Air.local>
2024-03-05 13:21:35 -0500

    ui front end render

--- public/index.js
+++ public/index.js
@@ -1,1 +1,39 @@
-console.info("started session");
+const body = document.querySelector("body");
+
+function createHeader() {
+  const header = document.createElement("header");
+  body.append(header);
+  return header;
+}
+
+function renderHeader() {
+  createHeader();
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  body.append(main);
+  return main;
+}
+
+function renderMain() {
+  createMain();
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  body.append(footer);
+  return footer;
+}
+
+function renderFooter() {
+  createFooter();
+}
+
+function renderElements() {
+  renderHeader();
+  renderMain();
+  renderFooter();
+}
+
+renderElements();
