a670c51ba2df7b4be2f0753d622987dd177b053f
William Gregorio <mail@williamgregorio.com>
2024-07-17 12:23:45 -0400

    logout() django redirect

--- main/dashboard/urls.py
+++ main/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'dashboard'
 urlpatterns = [
-    path('', views.dashboard_home, name='dashboard_home')
+    path('', views.dashboard_home, name='dashboard_home'),
+    path('logout/', views.dashboard_logout, name='dashboard_logout')
 ]
