9d4cb42c2ebd4ac27862c1d9c13898174a56a1de
William Gregorio <mail@williamgregorio.com>
2024-07-17 11:15:53 -0400

    dashboard home view intial setup on login required

--- main/dashboard/views.py
+++ main/dashboard/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+login_required(login_url='accounts:login')
+def dashboard_home(request):
+    return render(request, 'dashboard/index.html')
