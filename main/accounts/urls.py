98a0ebb2bd6178ef2d3f4e4eee3533ff91fe911c
William Gregorio <mail@williamgregorio.com>
2024-07-17 10:31:39 -0400

    replc on name for views conflict with login() std

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.home, name='home'), 
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup')
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup')
 ]
