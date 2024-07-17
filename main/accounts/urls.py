3cb7967e45b3402173c4fd80ac79f1d37ae16d02
William Gregorio <mail@williamgregorio.com>
2024-07-17 09:39:55 -0400

    urls addition for views - login and signup

--- main/accounts/urls.py
+++ main/accounts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'), 
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
