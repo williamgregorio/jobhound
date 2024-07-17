1bdec23321090280e36195f39a25a059d00181ae
William Gregorio <mail@williamgregorio.com>
2024-07-17 09:46:20 -0400

    raw signup, object create_user from User django and login post request redirection

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'accounts/index.html')
@@ -10,10 +10,19 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        #creation on objects - basic User
-
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
         return redirect('accounts:home')
     return render(request, 'accounts/signup.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("accounts:home")
     return render(request, 'accounts/login.html')
