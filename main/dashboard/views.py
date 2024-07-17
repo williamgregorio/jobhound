d2927d4a0bd4135dfa18089787ccb0022a0d2bcb
William Gregorio <mail@williamgregorio.com>
2024-07-17 12:17:42 -0400

    context infor views added

--- main/dashboard/views.py
+++ main/dashboard/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import JobEntry
 
-login_required(login_url='accounts:login')
+@login_required(login_url='accounts:login')
 def dashboard_home(request):
-    return render(request, 'dashboard/index.html')
+    context = {
+        'user_info': {
+            'username': request.user.username
+        }
+    }
+    return render(request, 'dashboard/index.html', context)
