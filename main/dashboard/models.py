47ea47d66ee053c645bf603ec6bbaf57acc0892f
William Gregorio <mail@williamgregorio.com>
2024-07-17 12:16:54 -0400

    job entry setup models

--- main/dashboard/models.py
+++ main/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class JobEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_entries')
+    company_name = models.CharField(max_length=255)
+    job_post_url = models.URLField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    applied = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.company}"
+    
+    class Meta:
+        ordering = ['-date_created']
