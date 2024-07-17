7c5f65a9a522e412f1d906b65ed175f42ff60734
William Gregorio <mail@williamgregorio.com>
2024-07-17 14:05:57 -0400

    models on resume temps and job entries

--- main/dashboard/models.py
+++ main/dashboard/models.py
@@ -13,3 +13,37 @@ class JobEntry(models.Model):
     
     class Meta:
         ordering = ['-date_created']
+
+class ResumeTemplate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resume_templates')
+    template_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=42)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f"{self.template_name}"
+
+class ResumeTemplateWorkExperience(models.Model):
+    resume_template = models.ForeignKey(ResumeTemplate, on_delete=models.CASCADE, related_name='work_experiences')
+    company_name = models.CharField(max_length=150)
+    job_title = models.CharField(max_length=150)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=42)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    currently_working = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.resume_template.template_name} - {self.company_name} ({self.job_title})"
+
+
+class ResumeTemplateWorkExperienceBulletPoint(models.Model):
+    work_experience = models.ForeignKey(ResumeTemplateWorkExperience, on_delete=models.CASCADE, related_name='bullet_points')
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.work_experience.resume_template.template_name} - {self.content}"
