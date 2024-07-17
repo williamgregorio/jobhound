5df327311a5ee517fd8cc478da885fc44a9c97bf
William Gregorio <mail@williamgregorio.com>
2024-07-17 14:07:41 -0400

    entries views and entry loggin

--- main/dashboard/views.py
+++ main/dashboard/views.py
@@ -12,6 +12,25 @@ def dashboard_home(request):
     }
     return render(request, 'dashboard/index.html', context)
 
+def dashboard_create_job_entry(request):
+    if request.method == 'POST':
+        company_name = request.POST['company_name']
+        job_post_url = request.POST['job_post_url']
+        applied_str = request.POST.get('applied', 'False')
+        applied = (applied_str == 'True')
+        job_entry = JobEntry.objects.create(
+            user=request.user,
+            company_name=company_name,
+            job_post_url=job_post_url,
+            applied=applied
+        )
+        return redirect('dashboard:dashboard_job_entries')
+    return render(request, 'dashboard/create_job_entry.html')
+
+def dashboard_job_entries(request):
+    job_entries = JobEntry.objects.filter(user=request.user)
+    return render(request, 'dashboard/job_entries.html', {'job_entries': job_entries})
+
 def dashboard_logout(request):
     logout(request)
     return redirect('accounts:home')
