2e833b47235b440f3852bb2f8cd0e0202033f2a1
William Gregorio <mail@williamgregorio.com>
2024-07-17 09:40:24 -0400

    post on signup user and password csrf addition on frontend

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
     return render(request, 'accounts/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #creation on objects - basic User
+
+        return redirect('accounts:home')
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
